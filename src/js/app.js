1704a716be988b7dd84e73d00b746033e2bba740
risrobs <robertivanov22@abv.bg>
2021-12-17 01:07:25 +0200

    m

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,19 @@
 import "../scss/app.scss";
-const R = require("ramda");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const filteredArray = numbersArray.filter(x => isEven(x))
+
+  filteredArray.forEach(el => appendLitoUl(el, ul))
+  console.log(filteredArray)
+
+  function appendLitoUl(content, ul) {
+    const liElement = document.createElement('li')
+    liElement.textContent = content
+    ul.appendChild(liElement)
+  }
 });
